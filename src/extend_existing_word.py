7a056718892d189cc712e232cd245b7420aaac91
timotheeMM <143833750+timotheeMM@users.noreply.github.com>
2025-01-06 19:02:07 +0100

    implement the row part of the extend_existing_word function

--- src/extend_existing_word.py
+++ src/extend_existing_word.py
@@ -2,7 +2,34 @@ from board import board
 
 
 def extend_existing_word(actual: list) -> list:
-    pass
+    possibilities = []
+    for i in range(len(board)):
+        j = 0
+        while j < len(board[i]):
+
+            if board[i][j] != 0 and j != len(board[i]) - 1 and board[i][j + 1] != 0:
+                word = board[i][j]
+                incr = 1
+                while True:
+                    if board[i][j + incr] != 0:
+                        if j + incr == len(board[i]) - 1:
+                            word += board[i][j + incr]
+                            j = len(board[i]) - 1
+                            break
+                        else:
+                            word += board[i][j + incr]
+                            incr += 1
+
+                    else:
+                        j += incr
+                        break
+
+                possibilities.append([word, ((i, j), (i, j + incr))])
+
+            else:
+                j += 1
+
+    print(possibilities)
 
 
 extend_existing_word(board)
